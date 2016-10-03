9d12aed13f4ec6878bb0428200627a230ce9f761
Banks Humphreys <iobg0320@gmail.com>
2016-10-03 10:19:54 -0500

    web sockets connecting and disconnecting

--- client/app/main.js
+++ client/app/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const socket=io();
-socket.on('connect',()=>{console.log('Socket connected',socket)})
+socket.on('connect',()=>{console.log('Socket connected',socket.id)})
 socket.on('disconnect',()=>{console.log('Socket Disconnected')})
 
 
